d4d0dcc8a1cc77e0939603d62454add6971e3d75
Viktor Sinelnykov <Fenya.ua@gmail.com>
2021-12-23 14:37:23 +0200

    fix onLeaveFeedback

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,10 +25,12 @@ export default function App() {
     if (option === 'good') {
       setGood(state => state + 1);
     }
-
-    if (option === 'neutral') setNeutral(state => state + 1);
-
-    if (option === 'bad') setBad(state => state + 1);
+    if (option === 'neutral') {
+      setNeutral(state => state + 1);
+    }
+    if (option === 'bad') {
+      setBad(state => state + 1);
+    }
   };
 
   const resetState = () => {
@@ -66,70 +68,3 @@ export default function App() {
     </div>
   );
 }
-
-// export default class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   countTotalFeedback() {
-//     const stateArrayValue = Object.values(this.state);
-//     const summFeedback = stateArrayValue.reduce((acc, value) => acc + value, 0);
-//     return summFeedback;
-//   }
-
-//   countPositiveFeedbackPercentage() {
-//     const summFeedback = this.countTotalFeedback();
-//     return Math.round((this.state.good / summFeedback) * 100);
-//   }
-
-//   onLeaveFeedback = option => {
-//     this.setState(prevState => {
-//       return {
-//         [option]: prevState[option] + 1,
-//       };
-//     });
-//   };
-
-//   resetState = () => {
-//     this.setState({
-//       good: 0,
-//       neutral: 0,
-//       bad: 0,
-//     });
-//   };
-
-//   render() {
-//     const stateArrayKeys = Object.keys(this.state);
-//     const countTotal = this.countTotalFeedback();
-//     const { good, neutral, bad } = this.state;
-//     const positivePercentage = this.countPositiveFeedbackPercentage();
-
-//     return (
-//       <div className={s.container}>
-//         <Section className={s.title} title="Please leave feedback">
-//           <FeedbackOptions
-//             options={stateArrayKeys}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//         </Section>
-//         <Section className={s.title} title="Statistics">
-//           {countTotal !== 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={countTotal}
-//               positivePercentage={positivePercentage}
-//               onReset={this.resetState}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
